ba797bf76903bbc97409c01bc28c6b7eee772c2d
Tanvir Anwar Rafi <rafil@tikweb.com>
2024-09-29 00:23:58 +0600

    update: code updated

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,4 +1,4 @@
 /// Android and iOS Bundle Ids
-const String APP_NAME = 'Bottom Navbar';
-const String ANDROID_ID = 'bd.tutorial.navbar';
-const String IOS_ID = 'bd.tutorial.navbar';
+const String APP_NAME = 'ToDo';
+const String ANDROID_ID = 'bd.tutorial.todo';
+const String IOS_ID = 'bd.tutorial.todo';
